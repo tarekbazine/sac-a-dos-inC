ee1370b7af35fdad1a4ffd59cb7fe6ac4eadf6be
terryBaz <et_bazine@esi.dz>
2017-10-21 19:38:56 +0100

    add algo

--- main.c
+++ main.c
@@ -10,12 +10,15 @@
 #define  N 5
 #define  w 9
 
-int int main(int argc, char const *argv[]) {
+int max(int a, int b);
 
-  /*algorithme sructures*/
+int main(int argc, char const *argv[]) {
+
+  /*algorithme's sructures*/
   int v[]={8,2,7,6,4},w[]={1,3,6,7,2},mat[N][W];
 
-  /*init matrice*/
+  /*init matrix*/
+  // omit this when you start j from 0
   for (int i = 0; i < N; i++) {
     mat[i][0]
   }
@@ -24,7 +27,29 @@ int int main(int argc, char const *argv[]) {
     mat[0][j]
   }
 
+  for (int i = 1; i < N; i++) {
+    for (int j = 1; j < W; j++) {
+        if (w[i]>j) {
+          mat[i][j]=m[i-1][j];
+        } else {
+          max(v[i]+m[i-1][j-w[i]]), m[i-1][j]);
+        }
+    }
+  }
 
+  //printing results
+  //matrix
+  for (int i = 1; i < N; i++) {
+    for (int j = 1; j < W; j++) {
+        printf("%d ", mat[i][j]);
+    }
+    printf("\n");
+  }
 
   return 0;
+}
+
+int max(int a,int b){
+    if(a>b) return a;
+    return b;
 }
