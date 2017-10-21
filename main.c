a8abd88a7dc8e8bf09664181de225278dea51f1f
terryBaz <et_bazine@esi.dz>
2017-10-21 19:10:08 +0100

    structures init

---
+++ main.c
@@ -0,0 +1,30 @@
+/***************************************
+***BAZINE Tarek - CHIKHAISSA Mahfoud****
+    ****ESI 2017/18 TPGO TP1****
+****************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+//those are going to be inputs from user
+#define  N 5
+#define  w 9
+
+int int main(int argc, char const *argv[]) {
+
+  /*algorithme sructures*/
+  int v[]={8,2,7,6,4},w[]={1,3,6,7,2},mat[N][W];
+
+  /*init matrice*/
+  for (int i = 0; i < N; i++) {
+    mat[i][0]
+  }
+  // todo optimize this
+  for (int j = 0; j < W; j++) {
+    mat[0][j]
+  }
+
+
+
+  return 0;
+}
