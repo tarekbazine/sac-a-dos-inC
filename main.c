e23c2b7cf4ec58be155fb4c3edf0d5ee75459077
terryBaz <et_bazine@esi.dz>
2017-10-21 20:08:39 +0100

    i won't work without an IDE ever !

--- main.c
+++ main.c
@@ -7,46 +7,48 @@
 #include <stdio.h>
 
 //those are going to be inputs from user
-#define  N 5
-#define  w 9
+#define N 5
+#define W 9
 
 int max(int a, int b);
 
 int main(int argc, char const *argv[]) {
 
-  /*algorithme's sructures*/
-  int v[]={8,2,7,6,4},w[]={1,3,6,7,2},mat[N][W];
+    /*algorithme's sructures*/
+    int v[]={8,2,7,6,4};
+    int w[]={1,3,6,7,2};
+    int mat[N][W];
 
-  /*init matrix*/
-  // omit this when you start j from 0
-  for (int i = 0; i < N; i++) {
-    mat[i][0]
-  }
-  // todo optimize this
-  for (int j = 0; j < W; j++) {
-    mat[0][j]
-  }
+    /*init matrix*/
+    // omit this when you start j from 0
+    for (int i = 0; i < N; i++) {
+        mat[i][0]=0;
+    }
+    // todo optimize this
+    for (int j = 0; j < W; j++) {
+        mat[0][j]=0;
+    }
 
-  for (int i = 1; i < N; i++) {
-    for (int j = 1; j < W; j++) {
-        if (w[i]>j) {
-          mat[i][j]=m[i-1][j];
-        } else {
-          max(v[i]+m[i-1][j-w[i]]), m[i-1][j]);
+    for (int i = 1; i < N; i++) {
+        for (int j = 1; j < W; j++) {
+            if (w[i]>j) {
+                mat[i][j]=mat[i-1][j];
+            } else {
+                mat[i][j]=max(v[i]+mat[i-1][j-w[i]], mat[i-1][j]);
+            }
         }
     }
-  }
 
-  //printing results
-  //matrix
-  for (int i = 1; i < N; i++) {
-    for (int j = 1; j < W; j++) {
-        printf("%d ", mat[i][j]);
+    //printing results
+    //matrix
+    for (int i = 1; i < N; i++) {
+        for (int j = 1; j < W; j++) {
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
     }
-    printf("\n");
-  }
 
-  return 0;
+    return 0;
 }
 
 int max(int a,int b){
